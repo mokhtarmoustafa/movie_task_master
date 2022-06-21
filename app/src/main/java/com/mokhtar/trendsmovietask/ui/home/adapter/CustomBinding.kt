38fc0b08f8c91a618f295deec611b19fd00494c2
mokhtarmoustafa <mokhtar.rnd@gmail.com>
2022-06-22 00:28:36 +0200

    update binding adapter

--- app/src/main/java/com/mokhtar/trendsmovietask/ui/home/adapter/CustomBinding.kt
+++ app/src/main/java/com/mokhtar/trendsmovietask/ui/home/adapter/CustomBinding.kt
@@ -6,28 +6,7 @@ import coil.load
 import coil.transform.RoundedCornersTransformation
 import com.mokhtar.trendsmovietask.R
 import com.mokhtar.trendsmovietask.util.ConfigurationStore
-import com.mokhtar.trendsmovietask.util.IMAGE_BASE_URL
 
-
-@BindingAdapter("base_url", "file_path", "image_size")
-fun ImageView.loadImageUrl(base_url: String, file_path: String, image_size: String) {
-
-    if (base_url.isNotEmpty()) {
-
-        val size = if (image_size == "1")
-            "w300"
-        else
-            "w500"
-
-        val completeUrl = "$base_url$size$file_path"
-        println("ImageView - $completeUrl")
-        load(completeUrl)
-        {
-            error(R.drawable.ic_place_holder)
-            crossfade(100)
-        }
-    }
-}
 
 @BindingAdapter("file_path")
 fun ImageView.loadImageUrl(file_path: String) {
